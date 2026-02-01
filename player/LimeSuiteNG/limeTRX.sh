0578f298ba68d8508ab7d780be843faed3e2b274
OSQZSS <osqzss@gmail.com>
2026-02-01 22:11:27 +0900

    Update shell scripts

--- player/LimeSuiteNG/limeTRX.sh
+++ player/LimeSuiteNG/limeTRX.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-limeTRX --input ../../afssim_iq16.bin --txPacketsInBatch=16 --rxPacketsInBatch=16 --log=verbose
+limeTRX.exe --input ../../afssim_iq16.bin --txPacketsInBatch=16 --rxPacketsInBatch=16 --log=verbose
