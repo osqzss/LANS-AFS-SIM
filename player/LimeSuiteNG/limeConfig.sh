5ba9d4da5245b0876a9112ad0fb98edbc625fde4
OSQZSS <osqzss@gmail.com>
2026-02-01 22:10:06 +0900

    Update shell scripts

--- player/LimeSuiteNG/limeConfig.sh
+++ player/LimeSuiteNG/limeConfig.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-limeConfig.exe --initialize --samplerate=12e6 --txen=1 --txlo=1575.42e6 --txlpf=12e6 --txpath=Band1 --txgain=10 --log=verbose
+limeConfig --initialize --samplerate=12e6 --txen=1 --txlo=1575.42e6 --txlpf=12e6 --txpath=Band1 --txgain=10 --log=verbose
 # for LimeSDR mini 2.0 with 20dB external attenuation
--- player/LimeSuiteNG/limeTRX.sh
+++ player/LimeSuiteNG/limeTRX.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-limeTRX.exe --input ../../afssim_iq16.bin --txPacketsInBatch=16 --rxPacketsInBatch=16 --log=verbose
+limeTRX --input ../../afssim_iq16.bin --txPacketsInBatch=16 --rxPacketsInBatch=16 --log=verbose
